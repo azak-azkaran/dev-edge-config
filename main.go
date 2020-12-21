8621124069903f68c36096079b9ff02d742f39c3
azak <azak@protonmail.com>
2020-12-21 10:37:31 +0100

    first restore after desaster
    * adds function to set Sugar
    * adds function FileExists
    * adds main function

--- main.go
+++ main.go
@@ -1,3 +1,48 @@
+package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	LogConfig = zap.NewProductionConfig()
+	Sugar     *zap.SugaredLogger = LogInit()
+)
+
+
+func LogInit() *zap.SugaredLogger{
+	var err error
+	LogConfig = zap.NewProductionConfig()
+	LogConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	LogConfig.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	LogConfig.Encoding = "console"
+	logger, err := LogConfig.Build()
+	if err != nil {
+		fmt.Println("Error building logger:", err)
+	}
+	defer logger.Sync() // flushes buffer, if any
+	return logger.Sugar()
+}
+
+
+func main (){
+	viper.SetConfigName("config.secret")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("~/")
+	viper.AddConfigPath("./")
+	//viper.SetConfigFile("./config.secret.yaml")
+
+	err:= viper.ReadInConfig()
+	if err != nil {
+		Sugar.Panic("ERROR while reading config file", err)
+	}
 
 	projects := viper.GetStringMap("project")
+	pw := viper.GetString("password")
+	Sugar.Info(pw)
+	Sugar.Info(projects)
+}
--- server.go
+++ server.go
@@ -15,6 +15,16 @@ type Server struct {
 	Status string
 }
 
+// Exists reports whether the named file or directory exists.
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
 func (server Server) SetId(id string) Server {
 	server.Id = id
 	Sugar.Debug("Setting ID to", server.Id)
